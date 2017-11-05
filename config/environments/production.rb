558c1512ef4afff1c89bf5b91882a9b90014d188
Xander Pronin <xanderpronin@gmail.com>
2017-11-05 12:09:29 +0300

    mailer confing for prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,19 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  #DEVISE
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'website.com',
+    user_name: 'xxx@xxx.com',
+    password: 'xxx',
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+
   # Attempt to read encrypted secrets from `config/secrets.yml.enc`.
   # Requires an encryption key in `ENV["RAILS_MASTER_KEY"]` or
   # `config/secrets.yml.key`.
