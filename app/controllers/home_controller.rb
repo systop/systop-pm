bfec8993d4ac7790cb0ebf8c7660437579b0936e
Xander Pronin <xanderpronin@gmail.com>
2018-01-16 22:36:23 +0300

    Added Exmo summ

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,24 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!
+include HomeHelper
 
   def index
+  	kv = AdminSetting.where(alias: "exmo").first.value.split "|"
+	exmo = Exmo.new(kv[0], kv[1])
+	user_info = exmo.request("user_info")
+	ticker = exmo.request('ticker')
+	exmo_rub = 0.0
+
+	user_info["balances"].each do |key,val|
+		unless val == "0"
+			# puts key + " = " + val
+			price = ticker["#{key}_RUB"]["avg"]
+			summ = val.to_f * price.to_f
+			summ_in_rubles = summ - summ*0.002
+			exmo_rub += summ_in_rubles
+		end
+	end
+	@yandex_rub = exmo_rub - exmo_rub*0.03
+
   end
 end
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,51 @@
+require 'net/https'
+require 'uri'
+require 'json'
 module HomeHelper
+
+class Exmo
+
+	def initialize(key,secret)
+		@KEY = key
+		@SECRET = secret
+	end
+
+	def request(method, params = nil)
+		params = {} if params.nil?
+		nonce = Time.now.strftime("%s%6N")
+		params['nonce'] = nonce
+		uri = URI.parse(['https://api.exmo.com/v1', method].join('/'))
+		post_data = URI.encode_www_form(params)
+		digest = OpenSSL::Digest.new('sha512')
+		sign = OpenSSL::HMAC.hexdigest(digest, @SECRET, post_data)
+		headers = {'Sign' => sign, 'Key'  => @KEY}
+		http = Net::HTTP.new(uri.host, uri.port)
+		req = Net::HTTP::Post.new(uri.path, headers)
+		req.body = post_data
+		http.use_ssl = true if uri.scheme == 'https'
+		response = http.request(req)
+		JSON.load response.body.to_s
+	end
+
+	def summ
+		
+	end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 end
